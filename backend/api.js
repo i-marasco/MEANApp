d5df1752600c13cecb9e617a6534f8dabd24cf06
Imarasco <ivan.marasco@icloud.com>
2026-09-19 14:18:23 +0200

    feat(security): Security fix

--- backend/api.js
+++ backend/api.js
@@ -1,26 +1,39 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 const cors = require('cors');
-var con = mysql.createConnection({
-    host: "database",
-    user: "root",
-    port: '3306',
-    password: "password",
-    database: "mean",
-    charset  : 'utf8'
+
+const con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    port: process.env.MYSQL_PORT,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    charset: 'utf8'
 });
-var corsOptions = {
+
+const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200
-}
+};
+
 con.connect(function(err) {
-    if(err) console.log(err);
+    if (err) {
+        console.log(err);
+    }
 });
+
 router.get('/books', cors(corsOptions), (req, res) => {
-    con.query("SELECT * FROM books", function (err, result, fields) {
-        if (err) res.send(err);
+    con.query('SELECT * FROM books', function(err, result, fields) {
+        if (err) {
+            return res.send(err);
+        }
+
         res.send(result);
         console.log(result);
     });
-}); module.exports = router;
+});
+
+module.exports = router;
